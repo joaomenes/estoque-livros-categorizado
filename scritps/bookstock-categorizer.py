a2447a8820029fb4bfe800d7c64ecc9907511102
Joao <joao.menes@hotmail.com>
2024-11-04 21:29:02 -0300

    Adicionando função

--- scritps/bookstock-categorizer.py
+++ scritps/bookstock-categorizer.py
@@ -1,11 +1,26 @@
 import json
 
-data_books = "dados/books_named.json"
+def categorie(locates: str) -> str:
 
-with open(data_books, "r") as file:
-    lendo = json.load(file)
-    #print(lendo)
+    data_books = "dados/books_named.json"
 
-dicionario_books = {books ['title'] : books ['category'] for books in lendo}
+    with open(data_books, "r") as file:
+        reading: list[dict, any] = json.load(file)            # é uma lista
 
-print(dicionario_books)
+    categories: list[str] = [book['category'] for book in reading]
+
+    if locates in categories:
+        return print(f'Há {categories.count(locates)} books nessa categoria.')
+    else: 
+        return print('Indisponível')
+
+try:
+    locates = str(input('Qual categoria deseja procurar? ')).upper()
+
+    if len(locates) != 1:
+        raise ValueError  # Levanta um erro se a entrada não for uma letra
+
+except ValueError:
+    print('Digite apenas uma letra.')
+
+categorie(locates)
